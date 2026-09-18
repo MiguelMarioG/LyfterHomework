91d4a770fa511bcab6802e8c65cfaefc5167fd90
Miguel Mario Guzman <miguelmarioguz@gmail.com>
2026-09-18 09:19:32 -0600

    adding the requested changes for the SQL exercises

--- databases/Exercise_SQL_Homework/Query_Exercise_SQL_4.sql
+++ databases/Exercise_SQL_Homework/Query_Exercise_SQL_4.sql
@@ -19,8 +19,16 @@ SELECT *
     FROM Products_Per_Invoice
     WHERE Products_Id = 4;
 
-SELECT Id, Products_Id, Total_Amount 
+-- Ya entendi mi error el problema era que el enunciado estaba pidiendo que
+-- Agrupara "GROUP BY" no que ordenadara "OORDER BY" ahi fue mi confusion e
+-- investigando me di cuenta que si quiero "GROUP BY" no debo incluir el "Id"
+-- ya que el Id no se repite y "Product Id" si lo hace y "SUM" suma el total
+-- de lo que cada "product Id" representa
+SELECT 
+    Products_Id, 
+    SUM(Total_Amount) AS Total_Purchased
     FROM Products_Per_Invoice
+    GROUP BY Products_Id
     ORDER BY Products_Id DESC;
 
 SELECT *
